4ad21a68aa099b8cee255c2d169e0af49dffe17e
Michael Cassidy <mikecassidy0000@gmail.com>
2018-10-29 16:16:53 -0400

    releases

--- client/src/components/releases/Releases.js
+++ client/src/components/releases/Releases.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Yasha from "../../img/YashaLanding.jpg";
+import Donnin from "../../img/max95.jpg";
 import { getReleases } from "../../actions/releaseActions";
 import "./Releases.css";
 
@@ -20,11 +21,27 @@ class Releases extends Component {
       releaseContent = releases.map(release => {
         const artist = release.artist;
         const title = release.title;
+        const listenLink = release.listenLink;
+        const buyLink = release.buyLink;
 
         return (
           <div className="release--card">
-            <div className="release--artist">{artist}</div>
-            <div className="release--title">{title} </div>
+            <div className="release--title">
+              {artist} - {title}{" "}
+            </div>
+            <img className="release--image" src={Donnin} alt="Max 95, Donnin" />
+            <div className="release--links">
+              <a
+                className="release--listen-link"
+                href={listenLink}
+                target="_blank"
+              >
+                Listen
+              </a>
+              <a className="release--buy-link" href={buyLink} target="_blank">
+                Buy
+              </a>
+            </div>
           </div>
         );
       });
