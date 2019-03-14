5dc6849ff3a2305fb1afa600235bb7422c41ce73
Michael Cassidy <mikecassidy0000@gmail.com>
2019-03-14 09:45:29 -0400

    add retail

--- client/src/pages/retail/RetailPage.js
+++ client/src/pages/retail/RetailPage.js
@@ -36,7 +36,15 @@ export default class RetailPage extends Component {
             <div className="retail-page--title">
               <div className="retail-page--title__text">RETAIL.</div>
             </div>
-            <div className="retail-page--carousel" />
+            <div className="retail-page--carousel">
+              <div className="retail--item">
+              <img className="retail--image" src="https://f4.bcbits.com/img/0015763424_36.jpg" alt="t-shirt"/>
+              <a href="https://orphanrecordsradio.bandcamp.com/merch/womanhood-long-sleeve-tee" target="_blank" rel="noopener noreferrer"><div className="retail--price">
+              $35
+              </div></a>
+            
+              </div>
+            </div>
           </div>
           {/* <Footer /> */}
         </div>
